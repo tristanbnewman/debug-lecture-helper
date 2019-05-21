ea1ea525ab0e23f4fcf63609b9b360d759ed1556
tristan <tristanb.newman@gmail.com>
2019-05-21 11:34:51 -0500

    initial commit fixed networking and setState bugs for get methods

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ class App extends Component {
   }
 
   getOne() {
-    axios.get(this.state.baseUrl + '/id/1/')
+    axios.get(this.state.baseUrl + '/1')
       .then(res => this.setState({ vehicles: res.data }))
   }
   getGreenVehicles() {
     axios.get(this.state.baseUrl + '?color=green')
-      .then(res => this.setState({ vehicles: res.data.vehicles }))
+      .then(res => this.setState({ vehicles: res.data }))
   }
   getFords() {
     axios.get(this.state.baseUrl + '?make=tesla')
